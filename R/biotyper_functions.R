5589f39c1203a6a9dc39c9ca84930435b1fbd994
cpauvert <cpauvert@users.noreply.github.com>
2025-06-26 11:04:40 +0200

    simplify biotyper cleaning function

--- R/biotyper_functions.R
+++ R/biotyper_functions.R
@@ -10,22 +10,7 @@ filter_identification <- function(biotyper_report, identification_threshold = 2)
 clean_biotyper_names <- function(biotyper_report){
   biotyper_report |> 
     dplyr::mutate(
-      name = stringr::str_remove(name, " ") %>% stringr::str_replace_all("DSM1", "DSM_1"),
-      name = purrr::reduce2(
-        c(
-          "BL-178-WT-3A$", "CLA-AA-H117$", "CLA-AA-H149$",
-          "CLA-AA-H222$", "CLA-AA-H284$", "CLA-JM-H23$",
-          "CLA-JM-H27$", "DSM_105335$", "H4-GB1-C9$",
-          "McC-252-APC-1B1$"
-        ),
-        c(
-          "BL-178-WT-3A_1", "CLA-AA-H117_1", "CLA-AA-H149_1",
-          "CLA-AA-H222_1", "CLA-AA-H284_1", "CLA-JM-H23_1",
-          "CLA-JM-H27_1", "DSM_105335_1", "H4-GB1-C9_1",
-          "McC-252-APC-1B1_1"
-        ),
-        .init = name,
-        stringr::str_replace_all
-      )
+      name = stringr::str_remove(name, " ") |> stringr::str_replace_all("DSM1", "DSM_1") |> 
+        stringr::str_replace_all("-", "_")
     )
 }
